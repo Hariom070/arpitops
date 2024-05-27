59dc3f96ae314f0e1e3cda2e47b7c9c64fc940a6
Arpita <arpitasingh190597@gmail.com>
2024-05-27 23:11:43 +0530

    ban ja

--- main.tf
+++ main.tf
@@ -2,3 +2,13 @@ resource "azurerm_resource_group" "arpitagit-rg" {
   name     = "arpitagit"
   location = "West Europe"
 }
+
+resource "azurerm_resource_group" "hari-rg21" {
+  name     = "arpita21"
+  location = "central india"
+}
+
+resource "azurerm_resource_group" "hari-rg22" {
+  name     = "arpita22"
+  location = "central india"
+}
